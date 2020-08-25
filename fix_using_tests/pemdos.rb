672a0c7d05670b528441fc183ba05a422f28cb72
Elias Delgado <evdelgado99@gmail.com>
2020-08-25 01:03:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,9 @@
 require 'pry'
 
-def snake_it_up(string, index)
+def snake_it_up(string)
   
-  index == string[0...8]
-  
-  if index[0] == "s"
-    10 * index[0] + string
+  if string[0] == "s"
+    10 * "s" + string
   else
     string
     
